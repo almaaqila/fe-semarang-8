2bf0b510555692b56e139abaa8049b92e5991c9d
almaaqila <almaaqila@gmail.com>
2023-09-23 18:12:17 +0700

    finalize html and css layouting

--- js/order.js
+++ js/order.js
@@ -22,24 +22,50 @@ if (window.location.pathname.endsWith('order.html')) {
     }
 }
 
-//mendefinisikan function untuk menampilkan pesan konfirmasi ketika submit
-function confirmSubmit() {
+// Mendefinisikan function untuk menampilkan pesan konfirmasi ketika submit
+function confirmationForm() {
     // Membuat const untuk menampilkan pesan konfirmasi
     const confirmation = confirm("Apakah Anda yakin untuk menyimpan formulir ini?");
-    
-    //Jika klik 'OK'
+
+    // Jika klik 'OK'
     if (confirmation) {
         // Select semua input element di order-form dan reset
-        const submitform = document.querySelector(".order-form");                
+        const submitform = document.querySelector(".order-form");
         submitform.reset();
 
         // Pesan telah berhasil mengirim form
         alert("Terima kasih telah membeli dari Pasar Segar");
-
         return true; // melakukan submit form
-
     } else {
-        //Jika klik 'cancel', return false untuk tidak submit form
+        // Jika klik 'cancel', return false untuk tidak submit form
         return false;
+    }
+}
+
+// Mendefinisikan function untuk submit form
+function submitForm() {
+    // Membuat const untuk input yang memiliki atribut "required"
+    const inputFields = document.querySelectorAll('input[required]');
+    // Membuat const untuk input select
+    const satuanSelect = document.getElementById("satuan");
+
+    // Menambahkan event listener ke inputFields
+    inputFields.forEach((input) => {
+        input.addEventListener('input', function () {
+            this.setCustomValidity(''); // Reset pesan validasinya
+        });
+
+        // Event listener ketika input kosong 
+        input.addEventListener('invalid', function () {
+            this.setCustomValidity('Data yang diisikan belum lengkap, silahkan lengkapi terlebih dahulu');
+        });
+    });
+
+    if (satuanSelect.value === "") { // Jika belum memilih satuan
+        alert("Harap pilih salah satu opsi pada Satuan.");
+        return false; // Mencegah submit form
+
+    } else { //Jika semua form tervalidasi
+        return confirmationForm(); // Memanggil fungsi konfirmasi submit form 
     }
 }
