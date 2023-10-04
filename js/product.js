81c22e1209140da44cc9d9b895b2f5619f1f0c6e
almaaqila <almaaqila@gmail.com>
2023-10-04 21:24:06 +0700

    add get method product for front-end

--- js/product.js
+++ js/product.js
@@ -1,6 +1,5 @@
 async function getProducts() {
-    // const res = await fetch('https://be-semarang-8.railway.internal/api/products');
-    const res = await fetch('http://localhost:3000/api/products');
+    const res = await fetch('https://be-semarang-8-production.up.railway.app/api/products');
 
     const restJSON = await res.json();
     const products = document.querySelector(".list");
@@ -8,10 +7,16 @@ async function getProducts() {
     if (restJSON.success) {
         for (let i = 0; i < restJSON.data.length; i++) {
             const div = document.createElement("div");
+            if(i != 0){
+                div.classList.add("card");
+            }
+            if(i % 3 == 0){
+                div.classList.remove("card");
+            }
             const product = `
             <img class="gambar" src=${restJSON.data[i].image} alt="" />
             <h3>${restJSON.data[i].name}</h3>
-            <p>${restJSON.data[i].weight}gr</p>
+            <p>${restJSON.data[i].weight} ${restJSON.data[i].satuan}</p>
             <div class="harga">
               <p>${restJSON.data[i].price}</p>
               <a class="pesan" href="order.html?produk=${restJSON.data[i].name}">Pesan</a>
